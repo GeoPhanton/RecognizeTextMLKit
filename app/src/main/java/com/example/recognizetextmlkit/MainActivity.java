8d142a4a28125a6630d271fee5845714b0efd6de
ghoostboy <brymv96@hotmail.com>
2021-02-11 23:51:37 -0500

    Open camera for take a photo

--- app/src/main/java/com/example/recognizetextmlkit/MainActivity.java
+++ app/src/main/java/com/example/recognizetextmlkit/MainActivity.java
@@ -2,10 +2,17 @@ package com.example.recognizetextmlkit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +21,45 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void doProcessCamera(View view) {
+        dispatchTakePictureIntent();
     }
 
     public void doProcessGallery(View view) {
+    }
+
+    private void dispatchTakePictureIntent() {
+        if(checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[] { Manifest.permission.CAMERA }, REQUEST_IMAGE_CAPTURE);
+        } else {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            Toast.makeText(MainActivity.this, "Listo", Toast.LENGTH_LONG).show();
+            startActivity(new Intent(MainActivity.this, RecognizeGallery.class).putExtra("image", imageBitmap));
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (requestCode == REQUEST_IMAGE_CAPTURE) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        } else {
+            if (requestCode == REQUEST_IMAGE_CAPTURE) {
+                Toast.makeText(MainActivity.this, "Debe aceptar los permisos para la cámara", Toast.LENGTH_LONG).show();
+            }
+        }
+        return;
     }
 }
