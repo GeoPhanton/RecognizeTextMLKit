4a896519d16feb77deaddfe166e398384814c936
Rafael-Guerrero-Moreira <rafael.guerrero2016@uteq.edu.ec>
2021-02-11 23:34:38 -0500

    Botones retroceder y refrescar añadidos

---
+++ app/src/main/java/com/example/recognizetextmlkit/RecognizeGallery.java
@@ -0,0 +1,14 @@
+package com.example.recognizetextmlkit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecognizeGallery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recognize_gallery);
+    }
+}
