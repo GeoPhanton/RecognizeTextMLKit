3ee22ef7493eecb57a4e5ec7a383c7ddd2e8feb3
Msuarezl <74911743+Msuarezl@users.noreply.github.com>
2021-02-11 23:10:29 -0500

    No action bar and OpenGallery

--- app/src/main/java/com/example/recognizetextmlkit/MainActivity.java
+++ app/src/main/java/com/example/recognizetextmlkit/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.recognizetextmlkit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Uri imageUri;
+    private static final int Gallery_Intent=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +22,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void doProcessGallery(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent,Gallery_Intent);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == Gallery_Intent) {
+            imageUri = data.getData();
+            Toast.makeText(this, "Imagen", Toast.LENGTH_LONG).show();
+        }
     }
 }
