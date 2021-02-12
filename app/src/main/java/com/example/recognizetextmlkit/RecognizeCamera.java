2298e185664f83748cec786c857e041a01cfa99f
Rafael-Guerrero-Moreira <rafael.guerrero2016@uteq.edu.ec>
2021-02-11 22:26:11 -0500

    Diseño Inicial pruebas y pruebas

--- app/src/main/java/com/example/recognizetextmlkit/RecognizeCamera.java
+++ app/src/main/java/com/example/recognizetextmlkit/RecognizeCamera.java
@@ -3,19 +3,12 @@ package com.example.recognizetextmlkit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class RecognizeCamera extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void doProcessCamera(View view) {
-    }
-
-    public void doProcessGallery(View view) {
+        setContentView(R.layout.activity_recognize_camera);
     }
 }
